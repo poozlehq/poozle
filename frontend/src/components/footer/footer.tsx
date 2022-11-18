84e9b3f451974a6ea0739943581429f1ea1115aa
Harshith Mullapudi <harshithmullapudi@gmail.com>
2022-11-18 20:23:44 +0530

    feat: added extension install/update/delete option

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,7 +1,9 @@
 /** Copyright (c) 2022, Poozle, all rights reserved. **/
 
-import { ActionIcon, Menu, ThemeIcon } from '@mantine/core';
-import { IconArchive, IconSettings } from '@tabler/icons';
+import { ActionIcon, Kbd, Menu, ThemeIcon } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
+import { IconArchive, IconInfoCircle, IconSettings } from '@tabler/icons';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import logoURL from '../../icons/icon.png';
@@ -13,6 +15,16 @@ interface FooterProps {
 
 export const Footer = ({ title }: FooterProps) => {
   const navigate = useNavigate();
+  const [opened, setOpened] = useState(false);
+
+  useHotkeys([
+    [
+      'ctrl+K',
+      () => {
+        setOpened(true);
+      },
+    ],
+  ]);
 
   return (
     <div className={styles.footer}>
@@ -24,18 +36,23 @@ export const Footer = ({ title }: FooterProps) => {
         {title && <div className={styles.title}>{title}</div>}
       </div>
 
-      <div>
-        <Menu shadow="md" width={200}>
+      <div className={styles.rightActions}>
+        <div className={styles.kbd}>
+          <Kbd>⌘</Kbd> + <Kbd>k</Kbd>
+        </div>
+        <Menu shadow="md" width={200} opened={opened}>
           <Menu.Target>
-            <ActionIcon color="gray" variant="filled">
+            <ActionIcon color="gray" variant="filled" onClick={() => setOpened(!opened)}>
               <IconSettings size={18} />
             </ActionIcon>
           </Menu.Target>
 
           <Menu.Dropdown>
-            <Menu.Label>Application</Menu.Label>
             <Menu.Item icon={<IconArchive size={16} />} onClick={() => navigate('/extensions')}>
               Extensions
+            </Menu.Item>
+            <Menu.Item icon={<IconInfoCircle size={16} />} onClick={() => navigate('/extensions')}>
+              About
             </Menu.Item>
           </Menu.Dropdown>
         </Menu>
