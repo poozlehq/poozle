93b8044b3c274419b0c2eaa811c9e45f2a1d2fae
Manoj K <saimanoj58@gmail.com>
2023-02-10 18:57:30 +0530

    fix: aws lambda open api schema

--- integrations/extensions/aws_lambda/src/index.ts
+++ integrations/extensions/aws_lambda/src/index.ts
@@ -1,6 +1,6 @@
 /** Copyright (c) 2023, Poozle, all rights reserved. **/
 import * as fs from "fs";
-import * as https from "https";
+// import * as https from "https";
 
 import * as aws4 from "aws4";
 
@@ -37,33 +37,46 @@ interface Spec {
   inputBlocks: Input[];
 }
 
-function request(opts: any) {
-  console.log(opts, "options");
-  https
-    .request(opts, (res) => {
-      res.pipe(process.stdout);
-    })
-    .end(opts.body || "");
-}
+// function request(opts: any) {
+//   console.log(opts, "options");
+//   https
+//     .request(opts, (res) => {
+//       res.pipe(process.stdout);
+//     })
+//     .end(opts.body || "");
+// }
 
 class GithubExtension {
-  getAuthHeaders(config: Config): Record<string, string | number> {
-    console.log(request(
-      aws4.sign({
-        service: "lambda",
-        region: "ap-south-1",
-        method: "GET",
-        path: "/2015-03-31/functions/",
-      })
-    ))
-    return aws4.sign(
-      {
-        service: "lambda",
-        region: "ap-south-1",
-        method: "GET",
-        path: "/2015-03-31/functions/",
-      }
-    );
+  getAuthHeaders(config: Config): Record<string, string> {
+    console.log(config)
+    
+    // const response = request(
+    //   aws4.sign({
+    //     service: "lambda",
+    //     region: "ap-south-1",
+    //     method: "GET",
+    //     path: "/2015-03-31/functions/",
+    //   },{
+    //   accessKeyId: 'AKIA2QVBJ6HSUSRQGLHO', 
+    //   secretAccessKey:'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'}
+    //   )
+    // )
+    // console.log(response.headers)
+    const response = aws4.sign({
+      service: "lambda",
+      region: "ap-south-1",
+      method: "GET",
+      path: "/2015-03-31/functions/",
+    },{
+    accessKeyId: 'AKIA2QVBJ6HSUSRQGLHO', 
+    secretAccessKey:'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'}
+    )
+    console.log(response)
+    return {
+      Authorization: response.headers.Authorization as string,
+      'X-Amz-Date': response.headers['X-Amz-Date'] as string,
+      Host: response.headers.Host as string
+    };
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
