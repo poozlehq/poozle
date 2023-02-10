92d35bb160c50225d278055a8284cfaade8ad3c1
Harshith Mullapudi <harshithmullapudi@gmail.com>
2023-02-10 19:56:27 +0530

    ext(AWS): fix auth-headers

--- integrations/extensions/aws_lambda/src/index.ts
+++ integrations/extensions/aws_lambda/src/index.ts
@@ -4,7 +4,7 @@ import * as fs from "fs";
 
 import * as aws4 from "aws4";
 
-type Config = Record<string, string | number>;
+type Config = any;
 
 const enum SchemaType {
   "GRAPHQL" = "GRAPHQL",
@@ -37,45 +37,24 @@ interface Spec {
   inputBlocks: Input[];
 }
 
-// function request(opts: any) {
-//   console.log(opts, "options");
-//   https
-//     .request(opts, (res) => {
-//       res.pipe(process.stdout);
-//     })
-//     .end(opts.body || "");
-// }
-
 class GithubExtension {
   getAuthHeaders(config: Config): Record<string, string> {
-    console.log(config)
-    
-    // const response = request(
-    //   aws4.sign({
-    //     service: "lambda",
-    //     region: "ap-south-1",
-    //     method: "GET",
-    //     path: "/2015-03-31/functions/",
-    //   },{
-    //   accessKeyId: 'AKIA2QVBJ6HSUSRQGLHO', 
-    //   secretAccessKey:'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'}
-    //   )
-    // )
-    // console.log(response.headers)
-    const response = aws4.sign({
-      service: "lambda",
-      region: "ap-south-1",
-      method: "GET",
-      path: "/2015-03-31/functions/",
-    },{
-    accessKeyId: 'AKIA2QVBJ6HSUSRQGLHO', 
-    secretAccessKey:'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'}
-    )
-    console.log(response)
+    const response = aws4.sign(
+      {
+        service: "lambda",
+        region: config.region as string,
+        method: config.context.method,
+        path: "/2015-03-31/functions/",
+      },
+      {
+        accessKeyId: config.accessKeyId as string,
+        secretAccessKey: config.secretAccessKey as string,
+      }
+    );
     return {
       Authorization: response.headers.Authorization as string,
-      'X-Amz-Date': response.headers['X-Amz-Date'] as string,
-      Host: response.headers.Host as string
+      "X-Amz-Date": response.headers["X-Amz-Date"] as string,
+      Host: response.headers.Host as string,
     };
   }
 
