b3bb25b8aa02a43bf062049542caf4022133063d
Harshith Mullapudi <harshithmullapudi@gmail.com>
2023-03-04 22:47:44 +0530

    Dynamic domain for graphql playground

--- engine-webapp/src/modules/playground/playground.tsx
+++ engine-webapp/src/modules/playground/playground.tsx
@@ -3,7 +3,9 @@
 import { useTheme } from '@graphiql/react';
 import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import { GraphiQL } from 'graphiql';
+import { useRouter } from 'next/router';
 import * as React from 'react';
+import { UserContext } from 'store/user_context';
 
 import { SideBarLayout } from 'layouts/sidebar_layout';
 import { AuthGuard } from 'wrappers/auth_guard';
@@ -14,8 +16,16 @@ import styles from './playground.module.scss';
 
 export function Playground() {
   const { setTheme } = useTheme();
+  const {
+    query: { workspaceId },
+  } = useRouter();
+  const { Workspace } = React.useContext(UserContext);
+  const currentWorkspace = Workspace.find(
+    (workspace) => workspace.workspaceId === workspaceId,
+  );
+
   const fetcher = createGraphiQLFetcher({
-    url: 'http://localhost:4000/graphql',
+    url: `https://graphql.poozle.dev/${currentWorkspace.slug}/graphql`,
     fetch,
   });
 
