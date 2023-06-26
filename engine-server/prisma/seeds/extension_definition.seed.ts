ac3cf25514e526bdde9d16865499eeb7f0d0ba58
Manoj K <saimanoj58@gmail.com>
2023-06-26 13:43:23 -0700

    add: docker compose

---
+++ engine-server/prisma/seeds/extension_definition.seed.ts
@@ -0,0 +1,87 @@
+import {
+  IntegrationType,
+  PrismaClient,
+  ReleaseStage,
+} from '@prisma/client';
+// import axios from 'axios';
+
+const prisma = new PrismaClient();
+
+type IntegrationDefinitions = {
+    [key: string]: {
+      name: string;
+      key: string;
+      icon: string;
+      sourceUrl: string;
+      releaseStage: string;
+      integrationType: string;
+    };
+  };
+
+async function main() {
+  const integrationDefinitions = await prisma.integrationDefinition.findMany({
+    where: {
+      workspace: null,
+    },
+  });
+
+  if (integrationDefinitions.length === 0) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // const extensionDefinitionsResponse: any = await axios.get(
+    //   'https://raw.githubusercontent.com/poozlehq/engine/main/public/extensions.json',
+    // );
+
+    // const extensionDefinitions = extensionDefinitionsResponse.data;
+
+    /**
+     * TODO(Manoj): move this to integrations.json
+     */
+    const integrationDefinitions: IntegrationDefinitions = {
+      github: {
+        name: 'Github',
+        key: 'github',
+        icon: 'github.svg',
+        sourceUrl:
+          'https://raw.githubusercontent.com/poozlehq/engine/ticketing/github/integrations/github/github/index.js',
+        releaseStage: 'ALPHA',
+        integrationType: 'TICKETING',
+      },
+    };
+
+    const integrationDefinitionCreate = Object.keys(integrationDefinitions).map(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (key) => {
+        const ed = {
+          ...integrationDefinitions[key],
+        };
+        return {
+          ...ed,
+          releaseStage: ReleaseStage[ed.releaseStage as ReleaseStage],
+          integrationType:
+            IntegrationType[ed.integrationType as IntegrationType],
+        };
+      },
+    );
+
+    const createdIntegrationDefinitions =
+      await prisma.integrationDefinition.createMany({
+        data: integrationDefinitionCreate,
+      });
+
+    console.log(createdIntegrationDefinitions);
+  }
+
+  /**
+   * TODO (harshith): Add more logic to add new integration from integrations.json
+   */
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ engine-server/prisma/seeds/index.ts
@@ -0,0 +1,4 @@
+
+// export all seeds from here
+
+export * from './extension_definition.seed';
