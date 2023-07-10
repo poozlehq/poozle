16ae5d30f5a647550f2a34415e3f30068b57ed3e
Harshith Mullapudi <harshithmullapudi@gmail.com>
2023-07-09 22:20:28 -0700

    Feat: added ats integrations

---
+++ integrations/ats/greenhouse/src/index.ts
@@ -0,0 +1,26 @@
+/** Copyright (c) 2023, Poozle, all rights reserved. **/
+
+import { BaseIntegration, GenericProxyModel, SpecificationResponse } from '@poozle/engine-idk';
+
+import spec from './spec';
+
+class GreenhouseIntegration extends BaseIntegration {
+  async spec(): SpecificationResponse {
+    return spec;
+  }
+
+  models() {
+    return [new GenericProxyModel()];
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function main(command: string, allParams: any) {
+  const integration = new GreenhouseIntegration();
+
+  const response = await integration.runCommand(command, allParams);
+
+  return response;
+}
+
+export default main;
