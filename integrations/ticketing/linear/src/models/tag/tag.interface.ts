105c4c80298e7cd6c090bd89ba4689d1afe89589
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2023-08-06 15:00:24 +0530

    Tag

--- integrations/ticketing/linear/src/models/tag/tag.interface.ts
+++ integrations/ticketing/linear/src/models/tag/tag.interface.ts
@@ -3,15 +3,38 @@
 import { Tag } from '@poozle/engine-idk';
 import { Meta } from 'common';
 
-export interface TagsResponse {
-  data: Tag[];
-  meta: Meta;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface TagWithRaw extends Tag {
   raw: any;
+}
+export interface TagsResponse {
+  data: TagWithRaw[];
+  meta: Meta;
 }
 
 export interface TagResponse {
-  data: Tag;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  raw: any;
+  data: TagWithRaw;
+}
+
+export interface IssueLabelCreateParams extends Tag {
+  team_id: string;
+  parent_id: string;
+}
+
+export interface TagCreateResponse {
+  lastSyncId: number;
+  success: string;
+  tag: TagWithRaw;
+}
+
+export interface GetTagParams {
+  pathParams: {
+    tag_id: number;
+  };
+}
+
+export interface IssueLabelUpdateParams {
+  pathParams: {
+    tag_id: number;
+  };
+  input: IssueLabelCreateParams;
 }
--- integrations/ticketing/linear/src/models/tag/tag.path.ts
+++ integrations/ticketing/linear/src/models/tag/tag.path.ts
@@ -1,35 +1,81 @@
 /** Copyright (c) 2023, Poozle, all rights reserved. **/
 
-import { BasePath, Config, Params, convertToRequestBody } from '@poozle/engine-idk';
+import { BasePath, Config, Params } from '@poozle/engine-idk';
 import axios, { AxiosHeaders } from 'axios';
 import { BASE_URL } from 'common';
 
-import { TagResponse } from './tag.interface';
-import { convertTag, tagMapping } from './tag.utils';
+import { GetTagParams, IssueLabelUpdateParams, TagResponse } from './tag.interface';
+import { convertTag } from './tag.utils';
 
 export class TagPath extends BasePath {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async getTag(headers: AxiosHeaders, _params: Params): Promise<TagResponse> {
-    /**
-     * TODO: You need to call the respective API return the data as expected by the type.
-     * You can check the github integration for reference.
-     */
+  async getTag(headers: AxiosHeaders, params: GetTagParams): Promise<TagResponse> {
+    try {
+      const id = params.pathParams?.tag_id;
+      const response = await axios({
+        url: `${BASE_URL}`,
+        headers,
+        data: {
+          query: `
+            query IssueLabel($issueLabelId: String!) {
+              issueLabel(id: $issueLabelId) {
+                id
+                name
+                color
+                description
+              }
+            }
+          `,
+          variables: {
+            id,
+          },
+        },
+      });
+      return { data: convertTag(response.data) };
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 
-  async updateTag(headers: AxiosHeaders, params: Params): Promise<TagResponse> {
-     /**
-     * TODO: You need to call the respective API return the data as expected by the type.
-     * You can check the github integration for reference.
-     */
+  async updateTag(headers: AxiosHeaders, params: IssueLabelUpdateParams): Promise<TagResponse> {
+    try {
+      const input = params.pathParams?.tag_id;
+      const response = await axios({
+        url: `${BASE_URL}`,
+        headers,
+        data: {
+          query: `
+            mutation IssueLabelUpdate($input: IssueLabelUpdateInput!, $issueLabelUpdateId: String!) {
+              issueLabelUpdate(input: $input, id: $issueLabelUpdateId) {
+                lastSyncId
+                issueLabel {
+                  id
+                  name
+                  color
+                  description
+                }
+                success
+              }
+            }
+          `,
+          variables: {
+            input,
+          },
+        },
+      });
+      return { data: convertTag(response.data) };
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 
-  async run(method: string, headers: AxiosHeaders, params: Params, config: Config) {
+  async run(method: string, headers: AxiosHeaders, params: Params, _config: Config) {
     switch (method) {
       case 'GET':
-        return this.getTag(headers, params);
+        return this.getTag(headers, params as GetTagParams);
 
       case 'PATCH':
-        return this.updateTag(headers, params);
+        return this.updateTag(headers, params as IssueLabelUpdateParams);
 
       default:
         throw new Error('Method not found');
