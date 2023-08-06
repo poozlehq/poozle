fb54f44dba142d806680505e9fecdcb2ea74ac91
Manoj K <saimanoj58@gmail.com>
2023-08-06 01:47:58 -0700

    Add Google Calendar integration

---
+++ engine-server/src/modules/categories/calendar/models/calendar/calendars.controller.ts
@@ -0,0 +1,83 @@
+/** Copyright (c) 2023, Poozle, all rights reserved. **/
+
+import { Body, Controller, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { IntegrationType } from '@prisma/client';
+import { Method, getDataFromAccount } from 'shared/integration_account.utils';
+
+import { IntegrationAccount } from '@@generated/integrationAccount/entities';
+
+import { GetIntegrationAccount } from 'common/decorators/integration_account.decorator';
+import { defaultQueryParams } from 'common/interfaces/defaults.constants';
+
+import { AuthGuard } from 'modules/auth/auth.guard';
+
+import {
+  CommonMessageQueryParams,
+  FreeBusyBody,
+  FreeBusyResponse,
+} from './calendars.interface';
+
+@Controller({
+  version: '1',
+  path: 'calendar',
+})
+@ApiTags('Calendar')
+@UseGuards(new AuthGuard())
+@ApiBadRequestResponse({
+  status: 400,
+  type: 'string',
+  description: 'Bad Request',
+})
+@ApiUnauthorizedResponse({
+  status: 401,
+  type: 'string',
+  description: 'Not authorised',
+})
+export class CalendarController {
+  @Post('freebusy')
+  async freebusy(
+    @Query() query: CommonMessageQueryParams,
+
+    @Body() FreeBusyBody: FreeBusyBody,
+    @GetIntegrationAccount(IntegrationType.MAIL)
+    integrationAccount: IntegrationAccount,
+  ): Promise<FreeBusyResponse> {
+    const freeBusyResponse = await getDataFromAccount(
+      integrationAccount,
+      `/freebusy`,
+      Method.POST,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      { ...defaultQueryParams, ...(query as any) },
+      {},
+      FreeBusyBody,
+    );
+
+    return freeBusyResponse;
+  }
+
+  @Post('availability')
+  async availability(
+    @Query() query: CommonMessageQueryParams,
+
+    @Body() AvailablityBody: FreeBusyBody,
+    @GetIntegrationAccount(IntegrationType.MAIL)
+    integrationAccount: IntegrationAccount,
+  ): Promise<FreeBusyResponse> {
+    const availabilityResponse = await getDataFromAccount(
+      integrationAccount,
+      `/availability`,
+      Method.POST,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      { ...defaultQueryParams, ...(query as any) },
+      {},
+      AvailablityBody,
+    );
+
+    return availabilityResponse;
+  }
+}
---
+++ integrations/calendar/google_calendar/src/common.ts
@@ -0,0 +1,18 @@
+/** Copyright (c) 2023, Poozle, all rights reserved. **/
+
+export const BASE_URL = 'https://www.googleapis.com/calendar/v3';
+
+export interface Meta {
+  previous: string;
+  next: string;
+  current: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getMetaParams(data: any[], limit: number, page: number) {
+  return {
+    previous: (page > 1 ? page - 1 : '').toString(),
+    current: page.toString(),
+    next: data.length === limit ? (page + 1).toString() : '',
+  };
+}
